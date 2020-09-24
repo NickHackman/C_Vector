5885bbbf23050286ba3ff06bfdba08b5c7e91d54
Nick Hackman <snickhackman@gmail.com>
2020-09-24 16:00:53 -0400

    Updated MIT License

--- src/vector.h
+++ src/vector.h
@@ -1,10 +1,10 @@
 /**************************************************************************************************
  * License: MIT *
  **************************************************************************************************
- * Copyright 2019 Scott Nicholas Hackman
+ * Copyright 2020 Scott Nicholas Hackman
  *
  * Permission is hereby granted, free of charge, to any person obtaining
- *copy of this software and associated documentation files (the "Software"),
+ * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
